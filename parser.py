9de117edbb1ceb107984b3a0718fae38d3940807
CamMitch4 <Cammitch21@gmail.com>
2025-06-18 16:04:09 +1000

    Add GeoLookup Functionality
    I have added the ability to show which city the IP addresses are from. General code clean up and smaller sample csv's too.

--- parser.py
+++ parser.py
@@ -2,10 +2,11 @@ import re
 import datetime
 import pandas as pd
 from collections import Counter
+import geoip2.database
 
 
 # File Reading
-file_path = "sample_logs/access.log"
+file_path = "sample_logs/access_sample.log"
 
 def read_log(file_path):
     with open(file_path, "r") as file:
@@ -54,6 +55,48 @@ df = pd.DataFrame(entries)
 df_404 = df[df['status'] == 404]
 ip_404_counts = df_404['ip'].value_counts()
 ip_404_df = ip_404_counts.reset_index()
-print(ip_404_df.head())
+ip_404_df.columns = ['ip','404_count']
 
-ip_404_df.to_csv('report.csv')
+ip_404_df.head(10).to_csv('reports/sample_report.csv')
+
+# GeoLite IP Location Lookup (Optional)
+
+geoip_path = 'data\GeoLite2-City.mmdb'
+
+reader = geoip2.database.Reader(geoip_path)
+
+def lookup_geo(ip):
+    try:
+        response = reader.city(ip)
+        return {
+            'country': response.country.name,
+            'city': response.city.name,
+            'latitude': response.location.latitude,
+            'longitude': response.location.longitude
+        }
+    except:
+        return{
+            'country': None,
+            'city': None,
+            'latitude': None,
+            'longitude': None
+        }
+        raise
+
+# Adding GeoLookup to Dataframe 
+unique_ips = df['ip'].dropna().unique()
+
+geo_data = {ip: lookup_geo(ip) for ip in unique_ips}
+
+df['country'] = df['ip'].map(lambda x: geo_data.get(x,{}).get('country'))
+df['city'] = df['ip'].map(lambda x: geo_data.get(x, {}).get('city'))
+
+# Outputting Dataframe to a csv
+df.head(10).to_csv('reports/sample_geo_report.csv', index=False)
+
+# Combining Suspicious activity with geolocation data
+geo_info = ip_404_df['ip'].apply(lookup_geo)
+
+final_df = pd.concat([ip_404_df, geo_info], axis=1)
+print(final_df)
+final_df.head(10).to_csv('reports/sample_suspicious_geo_report.csv')
